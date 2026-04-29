9ec8264993177cb1f724ea0a5b170f26ad831d1f
Harry Foster <Fosterh3@cardiff.ac.uk>
2026-04-29 16:35:56 +0100

    update alpha range violinplots with new data

---
+++ assets/img/alpha_range_violinplots/main.py
@@ -0,0 +1,108 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import dask.dataframe as dd
+
+from pathlib import Path
+
+here = Path(__file__).resolve()
+data_path = here.parents[2]
+
+dataframes_asp = [
+    data_path / "raw/aspiration_mu_eq_0.0/main.csv",
+    data_path / "raw/aspiration_mu_eq_0.1/main.csv",
+    data_path / "raw/aspiration_mu_eq_0.001/main.csv",
+    data_path / "raw/aspiration_mu_eq_0.005/main.csv",
+    data_path / "raw/aspiration_mu_eq_0.05/main.csv"
+]
+
+dataframes_fermi = [
+    data_path / "raw/fermi_mu_eq_0.1/main.csv",
+    data_path / "raw/fermi_mu_eq_0.001/main.csv",
+    data_path / "raw/fermi_mu_eq_0.005/main.csv",
+    data_path / "raw/fermi_mu_eq_0.05/main.csv"
+]
+
+dataframes_imispection = [
+    data_path / "raw/imispection_mu_eq_0.1/main.csv",
+    data_path / "raw/imispection_mu_eq_0.001/main.csv",
+    data_path / "raw/imispection_mu_eq_0.005/main.csv",
+    data_path / "raw/imispection_mu_eq_0.05/main.csv"
+]
+
+dataframes_introspection = [
+    data_path / "raw/introspection_mu_eq_0.0/main.csv",
+    data_path / "raw/introspection_mu_eq_0.1/main.csv",
+    data_path / "raw/introspection_mu_eq_0.001/main.csv",
+    data_path / "raw/introspection_mu_eq_0.005/main.csv",
+    data_path / "raw/introspection_mu_eq_0.05/main.csv"
+]
+
+dataframes_moran = [
+    data_path / "raw/moran_mu_eq_0.1/main.csv",
+    data_path / "raw/moran_mu_eq_0.001/main.csv",
+    data_path / "raw/moran_mu_eq_0.005/main.csv",
+    data_path / "raw/moran_mu_eq_0.05/main.csv"
+]
+
+dataframes = [
+    #dataframes_imispection,
+    #dataframes_fermi,
+    #dataframes_asp,
+    dataframes_introspection,
+    #dataframes_moran
+]
+
+def aggregate_dataframe(df):
+
+    aggregated_df = df.groupby("UID").agg(
+        p_C=("p_C", "first"),
+        alpha_min=("alpha_i", "min"),
+        alpha_max=("alpha_i", "max"),
+        N=("N", "first"),
+        process=("process", "first"),
+    )
+
+    aggregated_df["alpha_range"] = (
+        aggregated_df["alpha_max"] - aggregated_df["alpha_min"]
+    )
+
+    return aggregated_df.reset_index()
+
+
+
+for df_set in dataframes:
+    df_total = dd.read_csv(df_set)
+    df_total = aggregate_dataframe(df=df_total).compute()
+    process = df_total["process"].iloc[0]
+    for N, df in df_total.groupby("N"):
+        if N == 1:
+            continue
+        df["alpha_range_bin"] = pd.qcut(
+                    df["alpha_range"], q=10, precision=3, duplicates="drop"
+                )
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.set_ylim(0, 1)
+
+        groups = []
+
+        for _, g in df.groupby("alpha_range_bin", sort=True):
+            vals = g["p_C"].dropna().to_numpy()
+
+            if vals.size > 0:
+                groups.append(vals)
+
+        # HARD SAFETY CHECK
+        ax.violinplot(groups, showmeans=True)
+            
+        ax.set_title(f"{process}_N_eq_{N}")
+        folder = Path(here.parent / f"{process}_N_eq_{N}")
+        folder.mkdir(exist_ok=True)
+
+        ax.set_xlabel(r"decile of range($\alpha$)")
+        ax.set_ylabel(r"$p_C$")
+
+        plt.savefig(here.parent / f"{process}_N_eq_{N}/main.pdf")
+        plt.close()
