45f1018db07eef7e54e8ad7d5307ed2d00a5f3a2
Harry Foster <Fosterh3@cardiff.ac.uk>
2026-04-30 13:10:02 +0100

    Add figure generation

--- assets/img/hybrid_dynamics/main.py
+++ assets/img/hybrid_dynamics/main.py
@@ -9,6 +9,7 @@ import public_goods_games.contribution_rules
 here = pathlib.Path(__file__).resolve()
 assets_path = here.parents[2]
 
+
 def get_pc(transition_matrix, state_space, N):
     steady_state = ludics.compute_steady_state(transition_matrix)
     pc = (steady_state @ state_space).sum() / N
@@ -16,20 +17,27 @@ def get_pc(transition_matrix, state_space, N):
 
 
 def get_data(beta, epsilon, r, contribution_vector, split, N=8):
-    state_space = ludics.get_state_space(N=N,k=2)
+    state_space = ludics.get_state_space(N=N, k=2)
 
-    population_dynamic_array = np.concatenate([
-    np.array(
-        [ludics.compute_moran_transition_probability for _ in range(N-split)],
-        dtype=object
-    ),
-    np.array(
-        [ludics.compute_imitation_introspection_transition_probability for _ in range(split)],
-        dtype=object
+    population_dynamic_array = np.concatenate(
+        [
+            np.array(
+                [ludics.compute_moran_transition_probability for _ in range(N - split)],
+                dtype=object,
+            ),
+            np.array(
+                [
+                    ludics.compute_imitation_introspection_transition_probability
+                    for _ in range(split)
+                ],
+                dtype=object,
+            ),
+        ]
     )
-])
 
-    hybrid_population_dynamic = ludics.build_hybrid_population_dynamic(population_dynamic_array)
+    hybrid_population_dynamic = ludics.build_hybrid_population_dynamic(
+        population_dynamic_array
+    )
 
     transition_matrix = ludics.generate_transition_matrix(
         state_space=state_space,
@@ -39,29 +47,55 @@ def get_data(beta, epsilon, r, contribution_vector, split, N=8):
         r=r,
         selection_intensity=epsilon,
         choice_intensity=beta,
-        individual_to_action_mutation_probability=np.full(shape=(8,2), fill_value=0.05)
+        individual_to_action_mutation_probability=np.full(
+            shape=(8, 2), fill_value=0.05
+        ),
     )
-    pc = get_pc(transition_matrix=transition_matrix,state_space=state_space, N=N)
+    pc = get_pc(transition_matrix=transition_matrix, state_space=state_space, N=N)
     return pc
 
 
 parameter_sets = [
-    [7, 8, 0.5, 0.1, 'o'],
-    [10, 8, 0.8, 0.01, 'x'],
-    [15, 8, 0.1, 0.1, '^'],
-    [13, 8, 0.5, 0.1, 'v']
+    [7, 8, 0.5, 0.1, "o"],
+    [10, 8, 0.8, 0.01, "x"],
+    [15, 8, 0.1, 0.1, "^"],
+    [13, 8, 0.5, 0.1, "v"],
 ]
 fig, ax = plt.subplots()
 for r, M, beta, epsilon, marker in parameter_sets:
     data = []
-    for split in range(0,9):
-        print([public_goods_games.contribution_rules.linear_contribution_rule(i, 8, M) for i in range(8)])
-        contribution_vector = np.array([public_goods_games.contribution_rules.linear_contribution_rule(i, 8, M) for i in range(8)])
+    for split in range(0, 9):
+        print(
+            [
+                public_goods_games.contribution_rules.linear_contribution_rule(i, 8, M)
+                for i in range(8)
+            ]
+        )
+        contribution_vector = np.array(
+            [
+                public_goods_games.contribution_rules.linear_contribution_rule(i, 8, M)
+                for i in range(8)
+            ]
+        )
 
-        data.append(get_data(beta=beta, epsilon=epsilon, r=r, contribution_vector=contribution_vector, split=split))
-    ax.scatter(x=range(0,9), y=data, marker=marker, color='black', label=fr"$r$={r}, $\beta$={beta}, $\epsilon$={epsilon}")
-ax.axhline(y=0.5, color='black', linestyle='dashed')
+        data.append(
+            get_data(
+                beta=beta,
+                epsilon=epsilon,
+                r=r,
+                contribution_vector=contribution_vector,
+                split=split,
+            )
+        )
+    ax.scatter(
+        x=range(0, 9),
+        y=data,
+        marker=marker,
+        color="black",
+        label=rf"$r$={r}, $\beta$={beta}, $\epsilon$={epsilon}",
+    )
+ax.axhline(y=0.5, color="black", linestyle="dashed")
 ax.set_ylabel(r"$p_C$")
 ax.set_xlabel("Number of intrinsic players")
 plt.legend()
-plt.savefig(here.parent/"main.pdf")
+plt.savefig(here.parent / "main.pdf")
