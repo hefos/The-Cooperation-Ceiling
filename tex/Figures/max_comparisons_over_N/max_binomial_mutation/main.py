757f6830e5ed0a1d4329b36a3d1c7b038438792a
Harry Foster <Fosterh3@cardiff.ac.uk>
2026-04-30 13:57:28 +0100

    restore figures

---
+++ tex/Figures/max_comparisons_over_N/max_binomial_mutation/main.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from pathlib import Path
+
+here = Path(__file__).resolve()
+data_path = here.parents[3]
+
+dataframe_paths = [
+    data_path / "raw/binomial_population_fermi_mutation/main.csv",
+    data_path / "raw/binomial_population_moran_mutation/main.csv",
+    data_path / "raw/binomial_population_imispection_mutation/main.csv",
+    data_path / "raw/binomial_population_introspection_mutation/main.csv",
+]
+
+small_frames = []
+
+for df_path in dataframe_paths:
+    df = pd.read_csv(df_path, usecols=["N", "p_C", "process"])
+
+    df = df[df["N"].isin([3])]
+
+    small_frames.append(df)
+
+plot_df = pd.concat(small_frames, ignore_index=True)
+
+fig, ax = plt.subplots()
+
+sns.boxplot(data=plot_df, x="N", y="p_C", hue="process", width=0.5, ax=ax)
+
+plt.xlabel("N")
+plt.ylabel(f"$p_C$")
+
+plt.tight_layout()
+plt.savefig(here.parent / "main.pdf")
