4a04414744f1820411689f4fcfb58b7c94bf9174
Harry Foster <127303006+hefos@users.noreply.github.com>
2026-05-01 09:08:24 +0100

    Update draft paper (#119)
    * Initial update
    
    addresses multiple smaller TODOs and writes abstract.
    Also begins to update results section to consider mutation
    and adds alpha range violinplots
    
    * initial update
    
    works through most TODOs and makes a nice panel graph
    
    * Address TODOs
    
    * Add draft proof
    
    * Pre-chat with Vince commit
    
    * Update wording of proof
    
    * Update figure 1
    
    Makes figure 1 have a demonstration of the PGG, and 2 different views of the hypercube
    
    * Add attempt at proof. (#122)
    
    * Add attempt at proof.
    
    @hefos you need to check this through carefully.
    
    If this is right: I have added in the
    proof of the Lemma (from the KKO paper). This should probably be moved
    to supplementary materials (and in SI we probably need some definitions
    of terms, like stochastic dominance etc...).
    
    Likewise I think we should move the Moran and the Fermi result to after
    this proof.
    
    * Fix typos
    
    fixes the typos in the previous review
    
    * Add 5th condition.
    
    I also clean up the math delimiters.
    
    * Revert "Add 5th condition."
    
    This reverts commit 97f3ac8816fbde097218aa6cf5f64560c75f81e0.
    
    * Add TODO to capture problem in the proof.
    
    * Add a better version of condition (5).
    
    This closes #126 although we should still see if it is possible to not
    need this condition at all.
    
    ---------
    
    Co-authored-by: Harry Foster <Fosterh3@cardiff.ac.uk>
    
    * Remove binomial population (#128)
    
    * Rebase with most recent commit
    
    * Rebase onto e43eacb
    
    * Rebases removing references to binomial
    
    * Rebase removal of binomial from SI
    
    * Recompile pdf after rebase
    
    * Add extra figures (#123)
    
    * update figure 1
    
    * Add hybrid dynamics
    
    * Add hybrid dynamics figure
    
    * Add fully intrisic state to hybrid dynamics
    
    * update alpha range violinplots with new data
    
    * Add updated beta graph and alpha range violinplots
    
    * Add figure generation
    
    * Add data generation for sim comparisons
    
    * Add new comparison table data generation
    
    * remove old structure of process comparisons
    
    * replace old process comparisons with new data ones
    
    * add markov chains and mixing times to bib
    
    * Fix ruff
    
    * restore figures (#131)
    
    * restore figures
    
    * Change all references to new Figures
    
    * Remove old figures
    
    * Move SI to another dir
    
    * Update maximum comparison
    
    * fix ruff
    
    * Add url of docs
    
    ---------
    
    Co-authored-by: Vince Knight <vince@vknight.org>

---
+++ assets/img/hybrid_dynamics/main.py
@@ -0,0 +1,100 @@
+import matplotlib.pyplot as plt
+import pathlib
+import ludics
+import ludics.fitness_functions
+import numpy as np
+import public_goods_games.contribution_rules
+
+here = pathlib.Path(__file__).resolve()
+assets_path = here.parents[2]
+
+
+def get_pc(transition_matrix, state_space, N):
+    steady_state = ludics.compute_steady_state(transition_matrix)
+    pc = (steady_state @ state_space).sum() / N
+    return pc
+
+
+def get_data(beta, epsilon, r, contribution_vector, split, N=8):
+    state_space = ludics.get_state_space(N=N, k=2)
+
+    population_dynamic_array = np.concatenate(
+        [
+            np.array(
+                [ludics.compute_moran_transition_probability for _ in range(N - split)],
+                dtype=object,
+            ),
+            np.array(
+                [
+                    ludics.compute_imitation_introspection_transition_probability
+                    for _ in range(split)
+                ],
+                dtype=object,
+            ),
+        ]
+    )
+
+    hybrid_population_dynamic = ludics.build_hybrid_population_dynamic(
+        population_dynamic_array
+    )
+
+    transition_matrix = ludics.generate_transition_matrix(
+        state_space=state_space,
+        fitness_function=ludics.fitness_functions.heterogeneous_contribution_pgg_fitness_function,
+        compute_transition_probability=hybrid_population_dynamic,
+        contribution_vector=contribution_vector,
+        r=r,
+        selection_intensity=epsilon,
+        choice_intensity=beta,
+        individual_to_action_mutation_probability=np.full(
+            shape=(8, 2), fill_value=0.05
+        ),
+    )
+    pc = get_pc(transition_matrix=transition_matrix, state_space=state_space, N=N)
+    return pc
+
+
+parameter_sets = [
+    [7, 8, 0.5, 0.1, "o"],
+    [10, 8, 0.8, 0.01, "x"],
+    [15, 8, 0.1, 0.1, "^"],
+    [13, 8, 0.5, 0.1, "v"],
+]
+fig, ax = plt.subplots()
+for r, M, beta, epsilon, marker in parameter_sets:
+    data = []
+    for split in range(0, 9):
+        print(
+            [
+                public_goods_games.contribution_rules.linear_contribution_rule(i, 8, M)
+                for i in range(8)
+            ]
+        )
+        contribution_vector = np.array(
+            [
+                public_goods_games.contribution_rules.linear_contribution_rule(i, 8, M)
+                for i in range(8)
+            ]
+        )
+
+        data.append(
+            get_data(
+                beta=beta,
+                epsilon=epsilon,
+                r=r,
+                contribution_vector=contribution_vector,
+                split=split,
+            )
+        )
+    ax.scatter(
+        x=range(0, 9),
+        y=data,
+        marker=marker,
+        color="black",
+        label=rf"$r$={r}, $\beta$={beta}, $\epsilon$={epsilon}",
+    )
+ax.axhline(y=0.5, color="black", linestyle="dashed")
+ax.set_ylabel(r"$p_C$")
+ax.set_xlabel("Number of intrinsic players")
+plt.legend()
+plt.savefig(here.parent / "main.pdf")
